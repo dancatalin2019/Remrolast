a4d26373620bf01508913e0b610947391edbd13c
dancatalin2019 <arduino2019>
2019-11-05 09:26:56 +0200

    Repository updated

---
+++ src/main/java/repositoryCalculator/SumaRepository.java
@@ -0,0 +1,14 @@
+package repositoryCalculator;
+
+public class SumaRepository {
+
+
+
+    public int calculezSuma(int primul_nr, int al2lea_nr){
+            int sum;
+            sum=primul_nr+al2lea_nr;
+            return sum;
+
+
+    }
+}
